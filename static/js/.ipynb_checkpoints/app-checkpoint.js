ae0ac8c73010075150cb4dd4c73a92f42e722116
Alyssa H <alyssa_hondrade@icloud.com>
2023-10-20 15:07:30 +0800

    able to access json values now

--- static/js/.ipynb_checkpoints/app-checkpoint.js
+++ static/js/.ipynb_checkpoints/app-checkpoint.js
@@ -3,5 +3,28 @@ const samples_url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-cl
 
 // Fetch the JSON data
 d3.json(samples_url).then(function(data) {
-    console.log(data);
+    var names = Object.values(data.names);
+    var metadata = Object.values(data.metadata);
+    var samples = Object.values(data.samples);
+
+    // Sort the samples in descending order
+    // let descending_samples = samples.sort((first, second) => second.samples - first.samples);
+    // console.log(descending_samples);
+    for (let i=0; i<samples.length; i++) {
+        let row = samples[i]['sample_values'];
+        // Sort the samples in descending order
+        let descending_samples = row.sort(
+            (first, second) => second.row - first.row);
+        console.log(descending_samples);
+    };
 });
+
+
+
+// console.log(metadata);
+// // Sort the sample_values in descending order
+// let descending_samples = data.sort(
+//     (first, second) => second.sample_values - first.sample_values
+//     );
+
+// console.log(descending_samples);
