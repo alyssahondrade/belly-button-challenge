025553877447f824abd10db220c1f73d22be905b
Alyssa H <alyssa_hondrade@icloud.com>
2023-10-22 14:06:32 +0800

    isolated values for the first individual

--- static/js/app.js
+++ static/js/app.js
@@ -7,16 +7,29 @@ d3.json(samples_url).then(function(data) {
     var metadata = Object.values(data.metadata);
     var samples = Object.values(data.samples);
 
+    // Get the data for the first individual
+    let indiv_sample = samples[0]['sample_values'];
+    let indiv_ids = samples[0]['otu_ids'];
+    let indiv_labels = samples[0]['otu_labels'];
+
+
+
     // Sort the samples in descending order
     // let descending_samples = samples.sort((first, second) => second.samples - first.samples);
     // console.log(descending_samples);
-    for (let i=0; i<samples.length; i++) {
-        let row = samples[i]['sample_values'];
+    // for (let i=0; i<samples.length; i++) {
+    //     let row = samples[i];
+    //     let sample_values = row['sample_values'];
+    //     let otu_ids = row['otu_ids'];
+
+    //     console.log(sample_values, otu_ids);
+        
         // Sort the samples in descending order
-        let descending_samples = row.sort(
-            (first, second) => second.row - first.row);
-        console.log(descending_samples);
-    };
+        // let descending_samples = row.sort(
+        //     (first, second) => second.row - first.row);
+        // console.log(descending_samples);
+
+    // };
 });
 
 
